7211aa6b53d60efeae1905255c3210c29a598e75
[Himanshu Singh] <hs7875289@gmail.com>
2025-07-16 22:43:10 +0530

    tests

--- tests/test_audio_predictor.py
+++ tests/test_audio_predictor.py
@@ -1,3 +1,4 @@
+import numpy as np
 from unittest.mock import patch, MagicMock
 from src.pipeline.prediction_pipeline import AudioPredictor
 
@@ -6,7 +7,7 @@ from src.pipeline.prediction_pipeline import AudioPredictor
 @patch("src.pipeline.prediction_pipeline.FeatureExtractor")
 @patch("src.pipeline.prediction_pipeline.librosa.load")
 def test_predict(mock_load, mock_FE, mock_load_obj, mock_model_load):
-    mock_load.return_value = ([0.1] * 16000, 16000)
+    mock_load.return_value = (np.array([0.1] * 16000), 16000)
 
     mock_FE.return_value.extract_features.return_value = [0.1] * 15
     mock_load_obj.return_value.inverse_transform.return_value = ["Indian"]
